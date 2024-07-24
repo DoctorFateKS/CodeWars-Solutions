bc3d366dc24d3b6bff1c81044d8cc864646046cc
Kevin S. <saisonkevin92@gmail.com>
2024-07-24 14:52:26 +0200

    Create returning-strings.js
    Adding new challenge finished on CodeWars

---
+++ Javascript/returning-strings.js
@@ -0,0 +1,14 @@
+// Challenge Title: Returning Strings
+// Difficulty: 8 kyu
+// URL: https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+// Challenge Description:
+// Make a function that will return a greeting statement that uses an input; y
+// Your program should return, "Hello, <name> how are you doing today?".
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// Solution:
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
+
