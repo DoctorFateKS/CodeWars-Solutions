a007277d879e24558184a189416f45b73c4cc7c4
DoctorFateKS <kevin.saison91@gmail.com>
2024-09-11 20:54:12 +0200

    Add solution CodeWars

---
+++ Javascript/8-Kyu/convert-a-string-to-a-number.js
@@ -0,0 +1,25 @@
+/* Challenge Title: Counting Sheep...
+Difficulty: 8 kyu
+URL: https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+Challenge Description:
+We need a function that can transform a string into a number. 
+What ways of achieving this do you know?
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+My Solution: */
+
+function stringToNumber(str){
+    return parseInt(str);
+}
+
+// parseInt takes the string (str) and returns an integer.
+// If the string starts with numbers, it will convert them.
+// If the string contains non-numeric characters, parseInt will stop converting at the first non-numeric character.
+
+/* Example usage:
+
+"1234" --> 1234
+"605"  --> 605
+"1405" --> 1405
+"-7" --> -7 */
