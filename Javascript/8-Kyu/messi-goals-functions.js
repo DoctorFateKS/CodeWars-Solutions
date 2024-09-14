e6a3cc54566b119a91223a240d15d0c15db46d86
DoctorFateKS <kevin.saison91@gmail.com>
2024-09-14 21:59:53 +0200

    Add solution CodeWars

---
+++ Javascript/8-Kyu/messi-goals-functions.js
@@ -0,0 +1,19 @@
+/* Challenge Title: Grasshopper - Messi Goals Function
+Difficulty: 8 kyu
+URL: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/javascript
+
+Challenge Description:
+Messi is a soccer player with goals in three leagues:
+- La Liga
+- Copa Del Rey
+- Champions League
+Complete the function to return his total number of goals in all three leagues.
+
+My Solution: */
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals; // sum of goals in the three leagues
+  }
+
+/* Example usage:
+5, 10, 2  -->  17 */
