3c9b96d35101e6ad6e78edd0f90177bb882b4b2b
DoctorFateKS <kevin.saison91@gmail.com>
2024-09-20 20:23:59 +0200

    Add solution CodeWars

---
+++ Javascript/8-Kyu/you-cant-code-under-pressure-#1.js
@@ -0,0 +1,13 @@
+/* Challenge Title: You Can't Code Under Pressure #1
+Difficulty: 8 kyu
+URL: https://www.codewars.com/kata/53ee5429ba190077850011d4/javascript
+
+Challenge Description:
+Code as fast as you can!
+You need to double the integer and return it.
+
+My Solution: */
+
+function doubleInteger(i) {
+    return i * 2; 
+}
