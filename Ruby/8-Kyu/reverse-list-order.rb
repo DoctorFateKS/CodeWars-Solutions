722cd6b782b8bafe104544d3b80b9893c6d2d90a
DoctorFateKS <kevin.saison91@gmail.com>
2025-01-25 15:22:41 +0100

    Add new CodeWars solution

---
+++ Ruby/8-Kyu/reverse-list-order.rb
@@ -0,0 +1,12 @@
+# Challenge Title: Reverse List Order
+# Difficulty: 8 kyu
+# URL: https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+
+# Challenge Description:
+# In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+# My Solution: */
+
+def reverse_list list
+    list.reverse() # Reverse the elements of the list in-place
+end
